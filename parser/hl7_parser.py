1fd3a51eb9f7a35ada00e8cf3f82244b68cb4578
alcatrazxlr8 <saransh1000@gmail.com>
2025-05-09 03:07:38 -0500

    added sch segment parsing logic

--- parser/hl7_parser.py
+++ parser/hl7_parser.py
@@ -1,25 +1,22 @@
 ## core parsing logic
 import re
 
-
 filepath = "E:\Learn\hl7-parser\examples\example1.hl7"
 
 def read_hl7_file(filepath: str) -> str:
 	"""
 	Reads the content of an HL7 file and returns it as a raw string.
 	"""
-	# file = open("E:\Learn\hl7-parser\examples\example1.hl7", "r")
-	# message = file.read()
 	with open(filepath, "r", encoding="utf-8") as file:
 		message = file.read()
 		return message
 
 
 def message_parser(message) -> list[str]:
-	# segments = message.split("\r")
+	
 	segments = re.split(r'\r\n|\n|\r', message)
 	segments = [seg for seg in segments if seg.strip()]
-	# print(segments) ## debug print
+
 	return segments
 
 
@@ -30,16 +27,12 @@ def segment_parser(segments):
 
 	for segment in segments:
 		if segment.startswith("MSH") and "MSH" not in extracted_segments:
-			# msh = segment
 			extracted_segments["MSH"] = segment
 		elif segment.startswith("SCH") and "SCH" not in extracted_segments:
-			# sch = segment
 			extracted_segments["SCH"] = segment
 		elif segment.startswith("PID") and "PID" not in extracted_segments:
-			# pid = segment
 			extracted_segments["PID"] = segment
 		elif segment.startswith("PV1") and "PV1" not in extracted_segments:
-			# pv1 = segment
 			extracted_segments["PV1"] = segment
 
 	return extracted_segments
--- parser/segments.py
+++ parser/segments.py
@@ -7,3 +7,15 @@ Segment-specific parsers:
 
 from datetime import datetime
 from typing import Optional
+
+def parse_sch(segment: str) -> dict:
+
+	sch_fields = {"sch1": "appointment_id", "sch7": "appointment_datetime", "sch11": "location", "sch20": "reason"}
+	fields = segment.split("|")
+
+	sch_dict = {}
+	sch_dict[sch_fields["sch1"]] = fields[1].split("^")[0]
+	sch_dict[sch_fields["sch7"]] = datetime.strptime(fields[11], "%Y%m%d%H%M")
+	sch_dict[sch_fields["sch11"]] = fields[11]
+	sch_dict[sch_fields["sch20"]] = fields[20] | None
+	return sch_dict
